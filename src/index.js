58331eddac72ed9e85006eab8b947a93f1b1071d
iamsinchana <hssinchana04@gmail.com>
2025-11-17 08:38:04 +0530

    add changes

--- src/index.js
+++ src/index.js
@@ -12,6 +12,14 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`Server is running on port ${process.env.PORT || 8000}`);
+  })
+})
+.catch((err) => {
+  console.log("mongodb connection FAILED!!", err)
+})
 
 
 
